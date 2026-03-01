58117be83b6acededcaf280fb5d70a731413b4b8
Ashika <ashikav246@gmail.com>
2026-03-01 18:47:18 +0530

    UC4

---
+++ uc3.java
@@ -0,0 +1,12 @@
+public class OOPSString {
+    public static void main(String args[]) 
+    {
+        System.out.println(String.join("", "  *** ", " + ", " + ", "  ****** "));
+        System.out.println(String.join("", " ** **", " + ", " + ", " ** "));
+        System.out.println(String.join("", "*** **", " + ", " + ", "** "));
+        System.out.println(String.join("", "*** **", " + ", " + ", " ****** "));
+        System.out.println(String.join("", "*** **", " + ", " + ", "      ** "));
+        System.out.println(String.join("", " ** ** ", " + ", " + ", " ** ** "));
+        System.out.println(String.join("", "  **** ", " + ", " + ", "  ***** "));
+    }
+}
